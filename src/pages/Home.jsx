e7459fb4b915d34a9dfe2b4edd2517e4ecd56c93
Lavaruz <muzakiassami@gmail.com>
2024-03-27 10:56:59 +0700

    final

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,13 +7,21 @@ import Warning from "../components/Warning"
 import Manual from "../components/Manual"
 import Download from "../components/Download"
 import Footer from "../components/Footer"
-import { useState } from "react"
-import { useRef } from "react"
+import { useState, useRef, useEffect } from "react"
 
 export default function Home(){
     const [isVideoEnded, setIsVideoEnded] = useState(false);
+    const isMobile = useRef(window.innerWidth <= 768);
+    const isPlayed = localStorage.getItem("loadingScreen")
 
-    const isPlayed = true//localStorage.getItem("loadingScreen")
+    useEffect(() => {
+        // Tambahkan event listener untuk mengecek perubahan lebar layar
+        const handleResize = () => {
+            isMobile.current = window.innerWidth <= 768;
+        };
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
 
     function handleVideoEnd(e) {
         setIsVideoEnded(true);
@@ -26,12 +34,11 @@ export default function Home(){
 
     return(
         <>
-            {isPlayed !== "played" && 
+            {isPlayed !== "played" && !isMobile.current && 
             <>
-                <div className={`fixed z-50 left-0 right-0 justify-center bg-black flex ${isVideoEnded ? 'fadeOut' : ''}`} onAnimationEnd={handleAnimationEnd}>
-                    <video className="h-screen" id="video1" height="100" playsInline autoPlay muted onEnded={handleVideoEnd}>
+                <div className={`fixed z-50 left-0 right-0 top-0 bottom-0 justify-center bg-black flex ${isVideoEnded ? 'fadeOut' : ''}`} onAnimationEnd={handleAnimationEnd}>
+                    <video className="inlinevideo h-screen" id="video1" height="100" playsInline autoPlay muted onEnded={handleVideoEnd}>
                         <source src="/video/LoadingScreen.mp4" />
-                        Your browser does not support HTML5 video.
                     </video>
                 </div>
                 <div className={`fixed z-40 top-0 bottom-0 right-0 left-0 backdrop-blur-sm ${isVideoEnded ? 'fadeOutBlur' : ''}`} onAnimationEnd={handleAnimationEnd}></div>
